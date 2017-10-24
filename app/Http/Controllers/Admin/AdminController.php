6d9816ea35574ae6b8b80bab75f103deb08ad0b0
fuqiaoxin <fuqiaoxin@163.com>
2017-10-24 15:04:17 +0800

    first commit

---
+++ app/Http/Controllers/Admin/AdminController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+
+class AdminController extends Controller
+{
+    
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('home');
+    }
+}
---
+++ log.md
@@ -0,0 +1,8 @@
+// 获取当前路由实例
+$route = Route::current(); 
+
+// 获取当前路由名称
+$name = Route::currentRouteName();
+
+// 获取当前路由action属性
+$action = Route::currentRouteAction();
